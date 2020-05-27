6bdce9db1e28b10bb527ac1ab1c4afa9216fd96c
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-05-27 20:45:22 +0530

    Create 03 kLargestElements.cpp

---
+++ 16 Heaps/03 kLargestElements.cpp
@@ -0,0 +1,17 @@
+
+vector<int> kLargest(int arr[], int n, int k)
+{
+    vector<int> ans;
+    priority_queue<int> pq;
+    
+    for (int i = 0; i < n; i++)
+        pq.push(arr[i]);
+        
+    while (k--){
+        ans.push_back(pq.top());
+        pq.pop();
+    }
+    
+    return ans;
+    
+}
