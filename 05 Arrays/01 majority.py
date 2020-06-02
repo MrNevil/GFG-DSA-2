faea07e4cf151a104f15f8c04bd2aa4dc06f0255
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-06-02 23:53:39 +0530

    majority

--- 05 Arrays/01 majority.py
+++ 05 Arrays/01 majority.py
@@ -1,4 +1,9 @@
-#You are given an array arr[] of size N. You are also given two elements x and y. Now, you need to tell which element (x or y) appears most in the array. In other words, return the element, x or y, that has higher frequency in the array. If both elements have the same frequency, then just return the smaller element.
+/*
+You are given an array arr[] of size N. You are also given two elements x and y. 
+Now, you need to tell which element (x or y) appears most in the array. In other words, 
+return the element, x or y, that has higher frequency in the array. If both elements have the same frequency,
+then just return the smaller element.
+*/
 
 # NOTE :  We need to return the elements, not their counts.
 
